948ee5013150836242dd5a48778eba248202a4fa
Velmurugan <Velmuruganmecse@gmail.com>
2021-08-03 01:27:17 +0530

    Spring Framework

---
+++ src/main/java/com/vel/SpringWebAppDemo/SpringWebAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vel.SpringWebAppDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vel/SpringWebAppDemo/SpringWebAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vel.SpringWebAppDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
